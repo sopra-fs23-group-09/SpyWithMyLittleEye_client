2235f220e4d6d0f99064b79f3f75d7bb5d4e803a
xenawacker <xena.wacker@gmail.com>
2023-04-23 17:03:39 +0200

    Changed implementation of Hints and Guessing + trying to display a mapping of all guesses

--- src/components/views/Guessing.js
+++ src/components/views/Guessing.js
@@ -14,49 +14,54 @@ import {
     notifyHint, notifyStartTime,
     subscribe
 } from "../../helpers/stompClient";
+import {Button} from "../ui/Button";
+
 
 const StreetView = () => {
-  const mapRef = useRef(null);
+    const mapRef = useRef(null);
 
-  useEffect(() => {
-    const loader = new Loader({
-      apiKey: process.env.YOUR_API_KEY,
-      version: 'weekly',
-    });
+    useEffect(() => {
+        const loader = new Loader({
+            apiKey: process.env.YOUR_API_KEY,
+            version: 'weekly',
+        });
 
-    loader.load().then(() => {
-      const map = new window.google.maps.Map(mapRef.current, {
-        center: {lat: 47.373944, lng: 8.537667 },
-        zoom: 18,
-        streetViewControl: true,
-        fullscreenControl: false,
-      });
+        loader.load().then(() => {
+            const map = new window.google.maps.Map(mapRef.current, {
+                center: {lat: 47.373944, lng: 8.537667 },
+                zoom: 18,
+                streetViewControl: true,
+                fullscreenControl: false,
+            });
 
-      const streetView = map.getStreetView();
-      streetView.setPosition({ lat: 47.373944, lng: 8.537667 });
-      streetView.setVisible(true);
-      streetView.setOptions({
-          motionTracking: false,
-          clickToGo: false,
-          motionTrackingControl: false,
-          disableDefaultUI: true,
-          zoomControl: true,
-          gestureHandling: 'none',
-          addressControl: false,
-          linksControl: false,
-          panControl: false,
-          enableCloseButton: false,
-      });
-    });
-  }, []);
+            const streetView = map.getStreetView();
+            streetView.setPosition({ lat: 47.373944, lng: 8.537667 });
+            streetView.setVisible(true);
+            streetView.setOptions({
+                motionTracking: false,
+                clickToGo: false,
+                motionTrackingControl: false,
+                disableDefaultUI: true,
+                zoomControl: true,
+                gestureHandling: 'none',
+                addressControl: false,
+                linksControl: false,
+                panControl: false,
+                enableCloseButton: false,
+            });
+        });
+    }, []);
 
-  return (
-    <div
-      ref={mapRef}
-      style={{ height: '100%', width: '100%' }}
-    />
-  );
+    return (
+        <div
+            ref={mapRef}
+            style={{ height: '100%', width: '100%' }}
+        />
+    );
 };
+
+
+////////////////////////////////////////////////////////////////////////////////////////
 
 const FormField = props => {
     return (
@@ -76,27 +81,21 @@ FormField.propTypes = {
     onChange: PropTypes.func,
 };
 
+////////////////////////////////////////////////////////////////////////////////////////
+
 const Guessing = () => {
     const playerId = localStorage.getItem("userId");
     const lobbyId = localStorage.getItem("lobbyId");
     const token = localStorage.getItem("token");
 
-    //console.log("Lobby ID: " + lobbyId);
-    //console.log("Player ID: " + playerId);
-    //console.log("Token: " + token);
-
-    const [inputHint, setInputHint] = useState("");
-    const [inputGuess, setInputGuess] = useState("");
+    const [playerInput, setPlayerInput] = useState(null);
     const [hint, setHint] = useState("");
     const [guess, setGuess] = useState("");
-
-    const [role, setRole] = useState("");
-
+    const [guesses, setGuesses] = useState([]);
+    const [role, setRole] = useState(null);
     const [username, setUsername] = useState("");
-
     const [currentRound, setCurrentRound] = useState(null);
     const [amountOfRounds, setAmountOfRounds] = useState(null);
-
     const [timeLeft, setTimeLeft] = useState("");
 
     const distributeRole = async () => {
@@ -104,12 +103,29 @@ const Guessing = () => {
             const requestBody = JSON.stringify({playerId});
             const response = await api.get('/game/'+lobbyId+'/roleForUser/'+playerId, requestBody, {headers: {Token: token}});
             const role = response.data
-            setRole(role);
+            setRole("GUESSER");
         }  catch (error) {
-            alert(`Something went wrong during the login: \n${handleError(error)}`);
+            console.log("Couldn't fetch role\n" + handleError(error));
         }
 
     };
+
+    const makeSubscription = ()  => {
+        subscribeToHintInformation();
+        subscribeToGuessInformation();
+    }
+
+    useEffect(() => {
+        distributeRole();
+        displayCurrentRound();
+        if (!getConnection()) {
+            connect(makeSubscription);
+        }
+        else {
+            makeSubscription();
+        }
+
+    }, []);
 
     const displayCurrentRound = async () => {
         try {
@@ -119,120 +135,65 @@ const Guessing = () => {
             setCurrentRound(currentRound);
             setAmountOfRounds(amountOfRounds);
         }  catch (error) {
-            alert(`Something went wrong during the login: \n${handleError(error)}`);
+            console.log("Couldn't fetch round\n" + handleError(error));
         }
 
     };
 
-    const handleHintSubmit = () => {
-        console.log('hint submitted ✅');
-        setInputHint("");
-    }
 
-    const handleGuessSubmit = () => {
-        console.log('guess submitted ✅');
-        setInputGuess("");
-    }
+    const submitInput = () => {
+        if (role === "SPIER") {
+            notifyHint(lobbyId, playerInput);
+            console.log("Hint: " + playerInput);
+        }else if( role === "GUESSER"){
+            notifyGuess(lobbyId, playerId, playerInput);
+            console.log("Guess: " + playerInput);
+        }
+        setPlayerInput("");
 
-    useEffect(() => {
-        distributeRole()
-        displayCurrentRound();
-    }, []);
+    };
 
-    /*useEffect(() => {
-        const keyDownHandler = event => {
-            console.log('User pressed: ', event.key);
-
-            if (event.key === 'Enter' && role === "GUESSER") {
-                //console.log("THIS SHOULDNT GET DISPLAYED");
-                event.preventDefault();
-                /*if (inputGuess.trim() === "") {
-                    return;
-                }
-                setGuess(event.target.value);
-                console.log("SETTED GUESS: " + guess);
-
-                // 👇️ call submit function here
-                handleGuessSubmit();
-            }
-        };
-
-        document.addEventListener('keydown', keyDownHandler);
-
-        return () => {
-            document.removeEventListener('keydown', keyDownHandler);
-        };
-    }, [role]);*/
-
-    useEffect(() => {
-        const keyDownHandler = event => {
-            //console.log("WTF")
-            //console.log('User pressed: ', event.key);
-            console.log("ROLE: " + role);
-            if (event.key === 'Enter' && role === "SPIER") {
-                event.preventDefault();
-                /*if (inputHint.trim() === "") {
-                    return;
-                }*/
-                setHint(event.target.value);
-                console.log("HINT: " + hint);
-                // 👇️ call submit function here
-                handleHintSubmit();
-            }
-        };
-        document.addEventListener('keydown', keyDownHandler);
-
-        return () => {
-            document.removeEventListener('keydown', keyDownHandler);
-        };
-
-    }, [role]);
 
     function subscribeToHintInformation() {
         subscribe("/topic/games/" + lobbyId + "/hints",(response) => {
-            const hint = response["hint"];
-            setHint(hint);
+            const h = response["hint"];
+            console.log("hint received: " + h );
+            setHint(h);
         });
-        notifyHint(lobbyId, hint);
-    }
-    function subscribeToGuessInformation() {
-        subscribe("/topic/games/" + lobbyId + "/guesses",(response) => {
-            const guess = response["guess"];
-            const username = response["username"];
-            setUsername(username);
-            setGuess(guess);
-        });
-        notifyGuess(lobbyId, playerId, guess);
+
     }
 
-    function subscribeToTimeInformation() {
-        subscribe("/topic/games/" + lobbyId + "/startRound",(response) => {
-            const timeLeft = response["duration"];
-            setTimeLeft(timeLeft);
-            console.log("START TIME: " + timeLeft);
+
+    function subscribeToGuessInformation() {
+        subscribe("/topic/games/" + lobbyId + "/guesses",(response) => {
+            console.log("Response: " + response);
+            /*const g = response[0]["guess"];
+            console.log("guesses received: " + g );
+            const u = response[0]["guesserName"];
+            setUsername(u);
+            setGuess(g);*/
+
+            response.forEach(item => {
+                const g = item["guess"];
+                const u = item["guesserName"];
+                setGuesses(prevGuesses => [...prevGuesses, [u, g]]);
+            });
         });
-        notifyStartTime(lobbyId);
     }
 
     useEffect(() => {
-        connect(subscribeToHintInformation)
-    }, [hint]);
+       listGuesses();
 
-    /*useEffect(() => {
-        connect(subscribeToGuessInformation);
+    }, [guesses]);
 
-    }, [guess]);*/
-
-    /*useEffect(() => {
-        connect(subscribeToTimeInformation);
-
-    }, []);*/
-
+    const listGuesses = () => {
+        console.log("LIST WITH GUESSES: " + guesses);
+    }
 
     return (
         <BaseContainer>
-           <div className="code left-field">
-              <Icon icon="ph:eye-closed-bold" color="white" style={{ fontSize: '4rem'}}/>
+            <div className="code left-field">
+                <Icon icon="ph:eye-closed-bold" color="white" style={{ fontSize: '4rem'}}/>
             </div>
             <div className="base-container ellipse1">
             </div>
@@ -266,54 +227,63 @@ const Guessing = () => {
                             Hint: {hint}
                         </div>
                     </div>
-                    {(() => {
-                        if (guess === "CORRECT"){
-                            return (
-                                <div className="guessers correct-container">
-                                    <div className="guessers name">
-                                        {username}
-                                    </div>
-                                    <div className="guessers correct-guess">
-                                        GUESSED RIGHT
-                                    </div>
-                                </div>
-                            )
-                        }
-                        else if (guess !== "") {
-                            return (
-                                <div className="guessers wrong-container">
-                                    <div className="guessers name">
-                                        {username}
-                                    </div>
-                                    <div className="guessers wrong-guess">
-                                        {guess}
-                                    </div>
-                                </div>
-                            )
-                        }
-                    })()}
+                    <Button className="game-send-button"
+                            disabled={!playerInput}
+                            onClick={() => submitInput()}
+
+                    >
+                        <div className="guessing send-button-text">
+                            Send
+                        </div>
+                    </Button>
+                    {(() =>
+                        <div>
+                            {guesses.map((gs, index) => {
+                                if (gs[1] === "CORRECT"){
+                                    return (
+                                        <div key={index} className="guessers correct-container">
+                                            <div className="guessers name">
+                                                {gs[0]}
+                                            </div>
+
+                                            <div className="guessers correct-guess">
+                                                GUESSED RIGHT
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                                else {
+                                    return (
+                                        <div key={index} className="guessers wrong-container">
+                                            <div className="guessers name">
+                                                {gs[0]}
+                                            </div>
+
+                                            <div className="guessers wrong-guess">
+                                                {gs[1]}
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            })}
+                        </div>
+                    )}
                 </div>
                 {(() => {
+                    let pl ="" ;
                     if (role === "SPIER"){
-                        return (
-                            <FormField
-                                placeholder="Enter your hint..."
-                                value={inputHint}
-                                onChange={inputHint => setInputHint(inputHint)}
-
-                            />
-                        )
+                        pl = "Enter your hint..."
                     }
                     else if (role === "GUESSER"  && guess !== "CORRECT") {
-                        return (
-                            <FormField
-                                placeholder="Enter your guess..."
-                                value={inputGuess}
-                                onChange={inputGuess => setInputGuess(inputGuess)}
-                                //onSubmit={handleGuessSubmit}
-                            />
-                        )
+                        pl = "Enter your guess..."
                     }
+                    return (
+                        <FormField
+                            placeholder= {pl}
+                            value={playerInput}
+                            onChange={i => setPlayerInput(i)}
+                        />
+                    )
                 })()}
             </div>
         </BaseContainer>
