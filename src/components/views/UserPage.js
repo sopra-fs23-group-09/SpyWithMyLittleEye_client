cc437ebaec474c6c3fc008582b266a255b7f4c90
xenawacker <xena.wacker@gmail.com>
2023-05-15 15:03:11 +0200

    Adapted logo to vw one some pages + adapted UserPage to vw and vh

--- src/components/views/UserPage.js
+++ src/components/views/UserPage.js
@@ -137,7 +137,7 @@ const UserPage = () => {
   return (
       <BaseContainer>
              <Link to="/home" className="code left-field">
-                 <Icon icon="ph:eye-closed-bold" color="white" style={{ fontSize: '4rem' }} />
+                 <Icon icon="ph:eye-closed-bold" color="white" style={{ fontSize: '4vw' }} />
              </Link>
           <div className="base-container ellipse1">
           </div>
